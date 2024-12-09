0a1595975864b7aff3413b02ed051b395e4693b3
oriol-valentines <oriolvalentinesmiro@gmail.com>
2024-12-09 16:24:09 +0100

    Commit canvios iniciales
    Se ha cambiado la estructura del código para, ahora se ha hecho para poder hacer mas de una ronda, se ha agregado un bucle para reiniciar el tablero y se ha metido el random dentro del primer bucle ya que solo se va a generar una vez

--- src/tres_en_raya/Tres_en_raya.java
+++ src/tres_en_raya/Tres_en_raya.java
@@ -9,28 +9,21 @@ public class Tres_en_raya {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		
 		String[] nombres = {" "," "};
 		String[][] Posicion = {
 				{"-","-","-"},
 				{"-","-","-"},
 				{"-","-","-"}};
-		int Random = 0;
-		int contPos = 0;
 		int fila = 0;
 		int columna = 0;
-		int[][] insert = new int[fila][columna];
-		
 		Scanner x = new Scanner(System.in);
 		Random r = new Random();
-		
 		boolean jugar = true;
 		
 		//INSERTAR LOS NOPMBRES DE LOS JUGADORES 
 		for (int i = 0; i < nombres.length; i++) {
-		System.out.print("Introduce el nombre del jugador " + (i+1) + ": ");
-		nombres[i] = x.nextLine();
+			System.out.print("Introduce el nombre del jugador " + (i+1) + ": ");
+			nombres[i] = x.nextLine();
 		}
 		
 		//MOSTRAR LOS NOMBRES DE LOS JUGADORES 
@@ -42,34 +35,30 @@ public class Tres_en_raya {
 		}
 		System.out.println("__________________________________________");
 		
-		//REINICIAR EL TABLERO 
 		while(jugar) {
+			//REINICIAR EL TABLERO 
 			for (int i = 0; i < Posicion.length; i++) {
-				for (int j = 0; j < Posicion.length; j++) {
+				for (int j = 0; j < Posicion[i].length; j++) {
 					Posicion[i][j] = "-";
 				}
 			}
 			
-			for (int i = 0; i < Posicion.length; i++) {
-				for (int j = 0; j < Posicion[i].length; j++) {
-					System.out.print((contPos + 1) + " " +Posicion[i][j] + " ");
-					contPos++;
-				}System.out.println();
+			//DECIDIR QUIEN EMPIEZA 
+			int turno = r.nextInt(2);
+			System.out.print("La moneda ha decidido que es el turno de " + nombres[turno]);
+			
+			boolean partidaA = true;
+			
+			while(partidaA) {
+				//MOSTRAR EL TABLERO
+				System.out.println("El tablero: ");
+				for (int i = 0; i < Posicion.length; i++) {
+					for (int j = 0; j < Posicion[i].length; j++) {
+						System.out.print(Posicion[i][j] + " ");
+					}
+					System.out.println();
+				}
 			}
-			
-			//ESTE RANDOM GENERA UN NUMERO DEL 1 AL 2
-			Random = r.nextInt(2)+1;
-			
-			if (Random == 1){
-				System.out.print("En que posición quieres poner la ficha, jugador: " + nombres[0] + ": ");
-				insert[fila][columna] = x.nextInt();
-			}else {
-				System.out.print("En que posición quieres poner la ficha, jugador: " + nombres[1] + ": ");
-				insert[fila][columna] = x.nextInt();
-			}
-			
-			
-			
 		}
 		
 		
